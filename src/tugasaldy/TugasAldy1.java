b4ad72813597e1c01242234a4b4b3d75efe6b572
ambis123 <38097225+ambis123@users.noreply.github.com>
2018-04-06 23:16:04 +0700

    Membuat

--- src/tugasaldy/TugasAldy1.java
+++ src/tugasaldy/TugasAldy1.java
@@ -10,5 +10,21 @@ package tugasaldy;
  * @author Muhammad Rizaldy
  */
 public class TugasAldy1 {
+     String Nama;
+    String NPM;
+    String Kelas;
+    String Class;
+    String Atribut;
+    String Behavior;
+    String Objek;
     
+    void cetakInfo(){
+            System.out.println("Nama : "+Nama);
+            System.out.println("NPM : "+NPM);
+            System.out.println("Kelas : "+Kelas);
+            System.out.println("Class : "+Class);
+            System.out.println("Objek : "+Objek);
+            System.out.println("Atribut : "+Atribut);
+            System.out.println("Behavior : "+Behavior);
+    }
 }
