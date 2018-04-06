e8d371462a7129c6f194e6e4ab217afce2254074
ambis123 <38097225+ambis123@users.noreply.github.com>
2018-04-06 23:41:10 +0700

    mengisi

--- src/tugasaldy/TugasAldyAksi.java
+++ src/tugasaldy/TugasAldyAksi.java
@@ -11,7 +11,20 @@ package tugasaldy;
  */
 public class TugasAldyAksi {
     public static void main(String[] args) {
-        TugasAldy1 Abdi = new TugasAldy1();
+        TugasAldy1 Aldy = new TugasAldy1();
+        
+    Aldy.Nama="M.Rizaldy";
+    Aldy.NPM="16630387";
+    Aldy.Kelas="4A Nonreg";
+    Aldy.Class="Sekolah";
+    Aldy.Objek="Sekolah SD, Sekolah SMP, Sekolah SMA";
+    Aldy.Atribut="Sekolah SD= Meja: kayu, Kursi : kayu,Papan Tulis : Kapur"
+         + "Lantai Bangunan: 1 Lantai,Warna Atap : Biru,"
+         + "Sekolah SMP= Meja: Jati, Kursi: Jati, Papan Tulis: Spedol,"
+         + "Lantai Bangunan : 2 Lantai,Warna Atap: Merah,"
+         + "Sekolah SMA= Meja: Almunium, Kursi: Busa, Papan Tulis: TouchScreen,"
+         + " Lantai Bangunan: 3 Lantai,Warna Atap :Coklat";
+    Aldy.Behavior="Buat menuntut ilmu";
     
 }
 
